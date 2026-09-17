c42b23d56d5c22513a7c2879839b7a4c635c3f9a
survivinginheels <twofacedpromises@gmail.com>
2026-09-16 22:58:31 -0500

    Add jail command for moderating users

---
+++ src/commands/Moderation/jail.js
@@ -0,0 +1,108 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+
+import { successEmbed } from '../../utils/embeds.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+import { TitanBotError, ErrorTypes } from '../../utils/errorHandler.js';
+
+export default {
+  data: new SlashCommandBuilder()
+
+    .setName("jail")
+
+    .setDescription("Jail a user in the server")
+
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("The user to jail")
+        .setRequired(true)
+    )
+
+    .addStringOption((option) =>
+      option
+        .setName("reason")
+        .setDescription("Reason for the jail")
+        .setRequired(false)
+    )
+
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+
+  category: "moderation",
+
+  async execute(interaction, config, client) {
+
+    const user = interaction.options.getUser("target");
+
+    const reason =
+      interaction.options.getString("reason") ||
+      "No reason provided";
+
+    if (!user) {
+      throw new TitanBotError(
+        'Missing target user',
+        ErrorTypes.USER_INPUT,
+        'You must specify a user to jail.',
+        { subtype: 'invalid_user' }
+      );
+    }
+
+    if (user.id === interaction.user.id) {
+      throw new TitanBotError(
+        'Cannot jail self',
+        ErrorTypes.VALIDATION,
+        'You cannot jail yourself.'
+      );
+    }
+
+    if (user.id === client.user.id) {
+      throw new TitanBotError(
+        'Cannot jail bot',
+        ErrorTypes.VALIDATION,
+        'You cannot jail the bot.'
+      );
+    }
+
+    const member = await interaction.guild.members
+      .fetch(user.id)
+      .catch(() => null);
+
+    if (!member) {
+      throw new TitanBotError(
+        'User not found',
+        ErrorTypes.USER_INPUT,
+        'That user is not in this server.'
+      );
+    }
+
+    const jailRole = interaction.guild.roles.cache.find(
+      role => role.name === "Jailed"
+    );
+
+    if (!jailRole) {
+      throw new TitanBotError(
+        'Jailed role missing',
+        ErrorTypes.CONFIGURATION,
+        'The **Jailed** role does not exist. Please create it first.'
+      );
+    }
+
+    if (member.roles.cache.has(jailRole.id)) {
+      throw new TitanBotError(
+        'Already jailed',
+        ErrorTypes.VALIDATION,
+        `${user.tag} is already jailed.`
+      );
+    }
+
+    await member.roles.add(jailRole, reason);
+
+    await InteractionHelper.universalReply(interaction, {
+      embeds: [
+        successEmbed(
+          `🔒 **Jailed** ${user.tag}`,
+          `**Reason:** ${reason}`
+        ),
+      ],
+    });
+  },
+};
